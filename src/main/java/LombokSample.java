1c4e8c2256815b672d3c1369f527e1228afae8c7
Anna <krivitskaiaanna@gmail.com>
2025-07-12 12:12:38 +0300

    feature(hw02): add comments on lombok generated code

--- src/main/java/LombokSample.java
+++ src/main/java/LombokSample.java
@@ -1,11 +1,17 @@
 import lombok.*;
 import lombok.experimental.Accessors;
 
-//TODO: comments on what code is generated
 public class LombokSample {
     @SneakyThrows
     public static void handleException() {
         throw new IndexOutOfBoundsException("Boom!");
+
+//        generated code:
+//        try {
+//            throw new IndexOutOfBoundsException("Boom!");
+//        } catch (Throwable t) {
+//            throw lombok.Lombok.sneakyThrow(t);
+//        }
     }
 
     public static void main(String[] args) {
@@ -39,12 +45,47 @@ public class LombokSample {
         private String name;
         private Integer age;
         private String email;
+
+//        generated code:
+//        public Person(String name, Integer age, String email) {
+//            this.name = name;
+//            this.age = age;
+//            this.email = email;
+//        }
+//        public String getName() {
+//            return name;
+//        }
+//        public void setName(String name) {
+//            this.name = name;
+//        }
+//        public Integer getAge() {
+//            return age;
+//        }
+//        public void setAge(Integer age) {
+//            this.age = age;
+//        }
+//        public String getEmail() {
+//            return email;
+//        }
+//        public void setEmail(String email) {
+//            this.email = email;
+//        }
+//        public String toString() {
+//            return "Person(name=" + this.name + ", age=" + this.age + ", email=" + this.email + ")";
+//        }
     }
 
     @RequiredArgsConstructor
     public static class Car {
         private final String brand;
         private final String model;
+        private Integer year;
+
+//        generated code:
+//        public Car(String brand, String model) {
+//            this.brand = brand;
+//            this.model = model;
+//        }
     }
 
     @AllArgsConstructor
@@ -53,6 +94,14 @@ public class LombokSample {
         private String author;
         private Integer pages;
         private Float price;
+
+//        generated code:
+//        public Book(String title, String author, Integer pages, Float price) {
+//            this.title = title;
+//            this.author = author;
+//            this.pages = pages;
+//            this.price = price;
+//        }
     }
 
     @Builder
@@ -63,12 +112,48 @@ public class LombokSample {
         private final Float price;
     }
 
+//    generated code:
+//    public static class HouseBuilder {
+//        private String address;
+//        private Integer rooms;
+//        private Float area;
+//        private Float price;
+//
+//        public HouseBuilder address(String address) {
+//            this.address = address;
+//            return this;
+//        }
+//        public HouseBuilder rooms(Integer rooms) {
+//            this.rooms = rooms;
+//            return this;
+//        }
+//        public HouseBuilder area(Float area) {
+//            this.area = area;
+//            return this;
+//        }
+//        public HouseBuilder price(Float price) {
+//            this.price = price;
+//            return this;
+//        }
+//        public House build() {
+//            return new House(address, rooms, area, price);
+//        }
+//    }
+
     public static class Product {
         @Setter
         @Getter
         private String category;
         private String name;
         private Float price;
+
+//        generated code:
+//        public String getCategory() {
+//            return category;
+//        }
+//        public void setCategory(String category) {
+//            this.category = category;
+//        }
     }
 
     @Setter
@@ -78,5 +163,19 @@ public class LombokSample {
         private String url;
         private String password;
         private String login;
+
+//        generated code:
+//        public Configuration setUrl(String url) {
+//            this.url = url;
+//            return this;
+//        }
+//        public Configuration setPassword(String password) {
+//            this.password = password;
+//            return this;
+//        }
+//        public Configuration setLogin(String login) {
+//            this.login = login;
+//            return this;
+//        }
     }
 }
